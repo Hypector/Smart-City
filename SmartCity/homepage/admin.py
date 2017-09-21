23767908a54d28475ff44c526791a3c6a3b2f508
Hypector <30400705+Hypector@users.noreply.github.com>
2017-09-21 15:49:58 +1000

    Add files via upload

--- SmartCity/homepage/admin.py
+++ SmartCity/homepage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# from homepage.models import user
+from homepage.models import attraction
 
+# admin.site.register(user)
+admin.site.register(attraction)
 # Register your models here.
