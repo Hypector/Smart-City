90fb0cb46465e3a149da74a581af6ce2de2f4906
Hypector <30400705+Hypector@users.noreply.github.com>
2017-09-22 20:37:42 +1000

    Applied the Attractions Table to homepage

--- SmartCity/homepage/admin.py
+++ SmartCity/homepage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from homepage.models import Attraction
+
+admin.site.register(Attraction)
 
 # Register your models here.
--- SmartCity/homepage/models.py
+++ SmartCity/homepage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Attraction(models.Model):
+	name = models.CharField(max_length=30)
+	streetNum = models.IntegerField()
+	StreetName = models.CharField(max_length=30)
+	suburb = models.CharField(max_length=30)
+	areaCode = models.IntegerField()
+	website = models.URLField(max_length=30)
+	attractionType = models.CharField(max_length=30)
+	rating = models.IntegerField()
+
+	def __str__(self):
+		return self.title
 # Create your models here.
