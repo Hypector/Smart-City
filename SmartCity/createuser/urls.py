1352eea36051e82d54d0412f619f16d68dda65b2
whatbutter <owen_park@hotmail.com>
2017-10-23 11:07:05 +1000

    Commit
    Commit

--- SmartCity/createuser/urls.py
+++ SmartCity/createuser/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.createuser, name = 'createuser'),
+    url(r'^$', views.signup, name = 'CreateUser'),
     ]
--- SmartCity/createuser/views.py
+++ SmartCity/createuser/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-
+from createuser.forms import SignUpForm
 # Create your views here.
+
+
+
 def createuser(request):
 	return render(request, 'createuser/createuser.html')
+
+def signup(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username = username, password=raw_password)
+			login(request,user)
+			return redirect('home')
+
+	else:
+		form = SignUpForm()
+	return render(request, 'createuser.html', {'form': form})
 
